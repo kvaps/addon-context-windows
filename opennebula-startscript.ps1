55ab84f175c44bc241ed841e694c1b24d8670f46
kvaps <kvapss@gmail.com>
2016-04-08 18:07:15 +0300

    add: CreateShortCut function

--- opennebula-startscript.ps1
+++ opennebula-startscript.ps1
@@ -4,7 +4,7 @@ $Share = ""\\server.domain.tld\share"
 $User = "\user"
 $Password = "password"
 $Printers = @("\\server.domain.tld\printer1", "\\server.domain.tld\printer2")
-
+CreateShortCut "C:\Program Files\Internet Explorer\iexplore.exe" "https://google.com" "$Home\Desktop\Google.lnk"
 
 Remove-Item "C:\TEMP\.opennebula-startscript-is-done.txt"
 
@@ -18,8 +18,16 @@ function Map-Printers($Printers) {
     (New-Object -ComObject WScript.Network).AddWindowsPrinterConnection($Printer)
   }
 }
-
 Map-Printers -Printers $Printers
+
+# Create Shortcut
+function CreateShortCut( [string]$SourceExe, [string]$ArgumentsToSourceExe, [string]$DestinationPath ) {
+  $WshShell = New-Object -comObject WScript.Shell
+  $Shortcut = $WshShell.CreateShortcut($DestinationPath)
+  $Shortcut.TargetPath = $SourceExe
+  $Shortcut.Arguments = $ArgumentsToSourceExe
+  $Shortcut.Save()
+}
 '
 
 $SCRIPT | Out-File 'C:\.opennebula-userscript.ps1'
